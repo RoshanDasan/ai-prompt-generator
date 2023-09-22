237847d3dc7afaf3fd75cb471a64ef458c60680b
RoshanDasan <edroshan2001@gmail.com>
2023-09-22 20:18:14 +0530

    show-prompt-complete

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post, index) => (
+        <PromptCard key={index} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>
-      <h1>Feed</h1>
-    </div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [post, setPost] = useState([]);
 
-export default Feed
+  const handleSearchChange = (e) => {};
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      setPost(await response.json());
+    };
+    fetchPost();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
